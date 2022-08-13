78c328544a634af57b5eeb197b2d88f3328e49f6
戈多 <1130600679@qq.com>
2022-08-13 19:05:36 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+
+// 搜索引擎
+const engines = [
+    {name: '百度', engine: 'https://www.baidu.com/s?wd='},
+    {name: '谷歌', engine: 'https://www.google.com.hk/search?q='}
+];
+
+// 搜藏列表
+const collection = {
+    "娱乐": [
+        {
+            "name": "影视解析",
+            "engine": "https://svip.bljiex.cc"
+        }
+    ],
+    "实用": [
+        {
+            "name": "高清图库",
+            "engine": "https://unsplash.com"
+        }
+    ]
+};
+
+
+export {
+    engines,
+    collection
+}
