2d6aa5f2d29be5ff1382e1983b80c93418392057
戈多 <1130600679@qq.com>
2022-08-14 16:38:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,24 +2,24 @@
 // 搜索引擎
 const engines = [
     {name: 'ecosia', engine: 'https://www.ecosia.org/search?q='},
-    {name: 'baidu', engine: 'https://www.baidu.com/s?wd='},
     {name: 'google', engine: 'https://www.google.com.hk/search?q='},
+    {name: 'baidu', engine: 'https://www.baidu.com/s?wd='},
     {name: 'yandex', engine: 'https://yandex.com/search/?text='}
     
 ];
 
 // 搜藏列表
 const collection = {
-    "娱乐": [
-        {
-            "name": "影视解析",
-            "engine": "https://svip.bljiex.cc"
-        }
-    ],
     "实用": [
         {
             "name": "高清图库",
             "engine": "https://unsplash.com"
+        }
+    ],
+    "娱乐": [
+        {
+            "name": "影视解析",
+            "engine": "https://svip.bljiex.cc"
         }
     ]
 };
